96f6aac0ae77bd65a2b5199f47fe3f14a58ce231
Jack Bardoe <u5505198@warwick.ac.uk>
2023-11-27 22:42:44 +0000

    Solved the Towers of Hanoi

--- skeleton_halfway.c
+++ skeleton_halfway.c
@@ -3,6 +3,8 @@
 #include <time.h>
 #include <unistd.h>
 
+unsigned char location[4][2];
+
 void move_to_location(int connection, unsigned char id, unsigned char loc_h, unsigned char loc_l) {
 
 	unsigned char cs = ~ ( id + 0x07 + 0x03 + 0x1e + loc_l + loc_h + 0x30 + 0x00);
@@ -20,31 +22,84 @@ void wait_until_done(int connection) {
 	usleep(2000000);
 }
 
-void initialise(int connection) {
-	move_to_location(connection,1,0x01,0x90);
-	move_to_location(connection,2,0x01,0x40);
-	move_to_location(connection,3,0x01,0x90);
-	move_to_location(connection,4,0x01,0x01);
+void release(int connection) {
+	move_to_location(connection,5,0x01,0xff);
+	wait_until_done(connection);
+}
+
+void grip(int connection) {
 	move_to_location(connection,5,0x01,0x47);
 	wait_until_done(connection);
 }
 
-void up(int connection){
-	move_to_location(connection,1,0x01,0xff);
-	move_to_location(connection,2,0x01,0xff);
-	move_to_location(connection,3,0x01,0x4f);
-	move_to_location(connection,4,0x01,0xff);
-	move_to_location(connection,5,0x01,0xff);
+void dodge(int connection) {
+	move_to_location(connection,2,0x01,0x80);
 	wait_until_done(connection);
+
+	move_to_location(connection,1,0x01,0xff);
+	wait_until_done(connection);
+}
+
+void position1(int connection) {
+	dodge(connection);
+	move_to_location(connection,1,0x01,0x90);
+	move_to_location(connection,2,0x01,0x40);
+	move_to_location(connection,3,0x01,0x90);
+	move_to_location(connection,4,0x01,0x01);
+	wait_until_done(connection);
+	location[0][0] = 0x01;
+	location[0][1] = 0x90;
+	location[1][0] = 0x01;
+	location[1][1] = 0x40;
+	location[2][0] = 0x01;
+	location[2][1] = 0x90;
+	location[3][0] = 0x01;
+	location[3][1] = 0x01;
+}
+
+void position2(int connection) {
+	dodge(connection);
+	move_to_location(connection,1,0x01,0xff);
+	move_to_location(connection,2,0x01,0x40);
+	move_to_location(connection,3,0x01,0x90);
+	move_to_location(connection,4,0x01,0x01);
+	wait_until_done(connection);
+}
+
+void position3(int connection) {
+	dodge(connection);
+	move_to_location(connection,1,0x02,0x6e);
+	move_to_location(connection,2,0x01,0x40);
+	move_to_location(connection,3,0x01,0x90);
+	move_to_location(connection,4,0x01,0x01);
+	wait_until_done(connection);
+}
+
+void down(int connection){
+	move_to_location(connection,2,0x01,(location[1][1]-0x17));
+	move_to_location(connection,4,0x01,(location[3][1]+0x15));
+	wait_until_done(connection);
+	location[1][1] = location[1][1]-0x17;
+	location[3][1] = location[3][1]+0x15;
+}
+
+void move(int start, int depthStart, int end, int depthEnd) {
+	release(connection);
+	
 }
 
 int main(int argc, char* argv[]) {
 
 	int connection = open_connection("/dev/ttyUSB0",B1000000);
 
-	initialise(connection);
-
-	up(connection);
+	release(connection);
+	position2(connection);
+	position1(connection);
+	down(connection);
+	down(connection);
+	grip(connection);
+	position2(connection);
+	release(connection);
 	return 0;
 
 }
--- towersOfHanoi.c
+++ towersOfHanoi.c
@@ -3,6 +3,11 @@
 #include <time.h>
 #include <unistd.h>
 
+unsigned char location[4][2];
+unsigned char positions[3][2] = {{0x01,0x90}, {0x01,0xff}, {0x02,0x6e}};
+int depth[] = {0,3,3};
+
+
 void move_to_location(int connection, unsigned char id, unsigned char loc_h, unsigned char loc_l) {
 
 	unsigned char cs = ~ ( id + 0x07 + 0x03 + 0x1e + loc_l + loc_h + 0x30 + 0x00);
@@ -20,41 +25,83 @@ void wait_until_done(int connection) {
 	usleep(2000000);
 }
 
-void open(int connection) {
+void release(int connection) {
 	move_to_location(connection,5,0x01,0xff);
 	wait_until_done(connection);
 }
 
-void close(int connection) {
-	move_to_location(connection,5,0x01,0xff);
+void grip(int connection) {
+	move_to_location(connection,5,0x01,0x47);
 	wait_until_done(connection);
 }
 
-void move_to_position(int connection, unsigned char positions[4][2]) {//WIll move all actuators to the top positions of the pile (make sure the order is good)
-  //I think we should always start by turning motor 2 up slightly so it does not crash into other blocks
+void dodge(int connection) {
+	move_to_location(connection,2,0x01,0x80);
+	wait_until_done(connection);
 
-  //Then move to position
+	move_to_location(connection,1,0x01,0xff);
+	wait_until_done(connection);
+}
+
+void move_to_pile(int connection, unsigned char motor1[2]) {
+  	dodge(connection);
+
+	move_to_location(connection,1,motor1[0],motor1[1]);
+	wait_until_done(connection);
+	move_to_location(connection,2,0x01,0x40);
+	move_to_location(connection,3,0x01,0x90);
+	move_to_location(connection,4,0x01,0x01);
+	wait_until_done(connection);
+
+	location[0][0] = motor1[0];//TBC
+	location[0][1] = motor1[1];
+	location[1][0] = 0x01;
+	location[1][1] = 0x40;
+	location[2][0] = 0x01;
+	location[2][1] = 0x90;
+	location[3][0] = 0x01;
+	location[3][1] = 0x01;
+}
+
+void down(int connection){
+	move_to_location(connection,2,0x01,(location[1][1]-0x17));
+	move_to_location(connection,4,0x01,(location[3][1]+0x15));
+	wait_until_done(connection);
+	location[1][1] = location[1][1]-0x17;
+	location[3][1] = location[3][1]+0x15;
+}
+
+void move(int connection, int start, int depthStart, int end, int depthEnd) {
+	move_to_pile(connection, positions[start]);
+	int i;
+	for(i = 0; i<depthStart; i++){down(connection);}
+	grip(connection);
+	move_to_pile(connection, positions[end]);
+	int j;
+	for(j = 0; j<depthEnd; j++){down(connection);}
+	release(connection);
+}
+
+void solve(int connection, int numDisks, int start, int away, int end){
+    
+    if(numDisks == 0){
+        move(connection, start, depth[start]++, end, ((depth[end]--)-1));
+    }
+    else{
+        solve(connection, numDisks-1, start, end, away);
+        
+        move(connection, start, depth[start]++, end, ((depth[end]--)-1));
+        
+        solve(connection, numDisks-1, away, start, end);
+    }
 }
 
 int main(int argc, char* argv[]) {
 
 	int connection = open_connection("/dev/ttyUSB0",B1000000);
-
-	initialise(connection);//Initialises the robot arm to position 1 (may be removable)
-
-	unsigned char positions[8][4][2] = {
-    { {},{},{},{} }//The 4 motor positions for the top of pile 1
-    { {},{},{},{} }//The 4 motor positions for the middle of pile 1
-    { {},{},{},{} }//The 4 motor positions for the bottom of pile 1
-
-    { {},{},{},{} }//The 4 motor positions for the top of pile 2
-    { {},{},{},{} }//The 4 motor positions for the bottom of pile 2
-  
-    { {},{},{},{} }//The 4 motor positions for the top of pile 3
-    { {},{},{},{} }//The 4 motor positions for the middle of pile 3
-    { {},{},{},{} }//The 4 motor positions for the bottom of pile 3
-  }
-	
+	release(connection);
+	move_to_pile(connection, positions[0]);
+	solve(connection, 2, 0, 1, 2);
 	return 0;
 
 }
