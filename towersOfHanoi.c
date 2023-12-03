0ded6b97987d3b38e840c01df683c63c32197429
JBardoe <98495948+JBardoe@users.noreply.github.com>
2023-12-03 17:14:56 +0000

    Commented the code

--- towersOfHanoi.c
+++ towersOfHanoi.c
@@ -3,49 +3,57 @@
 #include <time.h>
 #include <unistd.h>
 
-unsigned char location[4][2];
-unsigned char positions[3][2] = {{0x01,0x90}, {0x01,0xff}, {0x02,0x6e}};
-int depth[] = {0,3,3};
+unsigned char location[4][2];//Stores the current location of the arm
+unsigned char positions[3][2] = {{0x01,0x90}, {0x01,0xff}, {0x02,0x6e}};//Stores the locations of the 3 piles
+int depth[] = {0,3,3};//Stores how low the arm should go for each move
 
-
+//Creates and sends a package to the robot arm telling it to move to a specified location
 void move_to_location(int connection, unsigned char id, unsigned char loc_h, unsigned char loc_l) {
 
-	unsigned char cs = ~ ( id + 0x07 + 0x03 + 0x1e + loc_l + loc_h + 0x30 + 0x00);
+	unsigned char cs = ~ ( id + 0x07 + 0x03 + 0x1e + loc_l + loc_h + 0x30 + 0x00);//Create the checksum
 
-	unsigned char arr[] = { 0xff, 0xff, id, 0x07, 0x03, 0x1e, loc_l, loc_h, 0x30, 0x00, cs };
+	unsigned char arr[] = { 0xff, 0xff, id, 0x07, 0x03, 0x1e, loc_l, loc_h, 0x30, 0x00, cs };//Create the package
 
 	int buff_len = 100;
-	unsigned char buff[buff_len];
+	unsigned char buff[buff_len];//Create the buffer location
 
-	int bytes_read = write_to_connection(connection,arr,11,buff,buff_len);
+	int bytes_read = write_to_connection(connection,arr,11,buff,buff_len);//Send the package
 
 }
 
+//Delay the code to allow time for the arm to reach its location
 void wait_until_done(int connection) {
 	usleep(2000000);
 }
 
+//Opens the claw
 void release(int connection) {
 	move_to_location(connection,5,0x01,0xff);
 	wait_until_done(connection);
 }
 
+//Closes the claw
 void grip(int connection) {
 	move_to_location(connection,5,0x01,0x47);
 	wait_until_done(connection);
 }
 
+//Moves the arm up and to a central position between moves so it does not knock any towers over
 void dodge(int connection) {
+	//Move up
 	move_to_location(connection,2,0x01,0x80);
 	wait_until_done(connection);
 
+	//Turn to the middle
 	move_to_location(connection,1,0x01,0xff);
 	wait_until_done(connection);
 }
 
+//Moves the arm over a certain pile
 void move_to_pile(int connection, unsigned char motor1[2]) {
-  	dodge(connection);
+  	dodge(connection);//Move out the way of any piles to avoid knocking them
 
+	//Update the registers to be at the top of a certain pile
 	move_to_location(connection,1,motor1[0],motor1[1]);
 	wait_until_done(connection);
 	move_to_location(connection,2,0x01,0x40);
@@ -53,7 +61,8 @@ void move_to_pile(int connection, unsigned char motor1[2]) {
 	move_to_location(connection,4,0x01,0x01);
 	wait_until_done(connection);
 
-	location[0][0] = motor1[0];//TBC
+	//Update the robots current position
+	location[0][0] = motor1[0];
 	location[0][1] = motor1[1];
 	location[1][0] = 0x01;
 	location[1][1] = 0x40;
@@ -63,6 +72,7 @@ void move_to_pile(int connection, unsigned char motor1[2]) {
 	location[3][1] = 0x01;
 }
 
+//Move the robot claw one block down a certain pile and update its position store
 void down(int connection){
 	move_to_location(connection,2,0x01,(location[1][1]-0x17));
 	move_to_location(connection,4,0x01,(location[3][1]+0x15));
@@ -71,37 +81,47 @@ void down(int connection){
 	location[3][1] = location[3][1]+0x15;
 }
 
+//Perform one move in the algorithm from a start position to an end position
 void move(int connection, int start, int depthStart, int end, int depthEnd) {
-	move_to_pile(connection, positions[start]);
+	move_to_pile(connection, positions[start]);//Go to the top of the start pile
+	
+	//Move down the pile to the required block
 	int i;
 	for(i = 0; i<depthStart; i++){down(connection);}
-	grip(connection);
-	move_to_pile(connection, positions[end]);
+	grip(connection);//Grab the block
+	
+	move_to_pile(connection, positions[end]);//Go to the top of the end pile
+
+	//Move down the pile to the required position
 	int j;
 	for(j = 0; j<depthEnd; j++){down(connection);}
-	release(connection);
+	release(connection);//Release the block
 }
 
-void solve(int connection, int numDisks, int start, int away, int end){
+//Implement a recursive algorithm to solve the towers of hanoi
+void solve(int connection, int numBlocks, int start, int away, int end){
     
-    if(numDisks == 0){
+    if(numBlocks == 0){
         move(connection, start, depth[start]++, end, ((depth[end]--)-1));
     }
     else{
-        solve(connection, numDisks-1, start, end, away);
+        solve(connection, numBlocks-1, start, end, away);
         
         move(connection, start, depth[start]++, end, ((depth[end]--)-1));
         
-        solve(connection, numDisks-1, away, start, end);
+        solve(connection, numBlocks-1, away, start, end);
     }
 }
 
 int main(int argc, char* argv[]) {
 
-	int connection = open_connection("/dev/ttyUSB0",B1000000);
+	int connection = open_connection("/dev/ttyUSB0",B1000000);//Connect to the arm
+	
+	//Initialise the arm's position
 	release(connection);
 	move_to_pile(connection, positions[0]);
-	solve(connection, 2, 0, 1, 2);
+	
+	solve(connection, 2, 0, 1, 2);//Solve the problem
 	return 0;
 
 }
